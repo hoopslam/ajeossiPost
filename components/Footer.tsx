72ef9d2871b85fac13e4cd0f74eec71834752bff
David <hoopslam@gmail.com>
2023-02-24 17:15:34 +0900

    chore: programmatically update year

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,9 @@
 function Footer() {
     return (
         <div className='flex p-5 justify-evenly items-center border-t-2 border-red-600'>
-            <p className='font-yellowtail'>&#169; David Ajeossi 2023</p>
+            <p className='font-yellowtail'>
+                &#169; {` David Cho ${new Date().getFullYear()}`}
+            </p>
             <p>For inquiries: hoopslam@gmail.com</p>
         </div>
     );
